3b5948da5b7e788f7883c92cc6a3e041efc609e2
webAqemu <notych2812@gmail.com>
2020-05-29 13:16:25 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -34,6 +34,14 @@ $(function () {
 		$('.icon-th-large').addClass('active');
 	});
 
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').slideToggle();
+	});
+
+	$('.header__btn-menu').on('click', function () {
+		$('.header__box').toggleClass('active');
+	});
+
 	var mixer = mixitup('.products__inner-box');
 
 
